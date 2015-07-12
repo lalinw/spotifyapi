67e6b46ec658d2ba3e15f3ea84ccb5baad6aee4a
Irene W <i.lalinw@gmail.com>
2015-07-12 03:58:00 -0700

    second changes

--- app.js
+++ app.js
@@ -1,23 +1,34 @@
 var data;
-var baseUrl = 'https://api.spotify.com/v1/search?type=track&query='
+var url;
 var app = angular.module('app', [])
+var toptracks;
+
+
+
 
 var ctrl = app.controller('ctrl', function($scope, $http) {
   $scope.audioObject = {}
-  $scope.getSongs = function() {
-    $http.get(baseUrl + $scope.track)
-      .success(function(response){
-        data = $scope.tracks = response.tracks.items
-      
-      })
+  
+
+  $scope.getResults = function() {
+    url = 'https://api.spotify.com/v1/search?type=' + $scope.searchType + '&query=' + $scope.search;
+    //get url based on whether they are searhcing by track or artist
+    $http.get(url).success(function(response){
+      if ($scope.searchType == 'track') {
+        data = $scope.results = response.tracks.items;
+      } else {
+        data = $scope.results = response.artists.items;
+      }
+      //resulting array is initialized here
+    })
   }
+
   $scope.play = function(song) {
     if($scope.currentSong == song) {
       $scope.audioObject.pause()   
       $scope.currentSong = false
       return
-    }
-    else {
+    } else {
       if($scope.audioObject.pause != undefined) {
         $scope.audioObject.pause()
       }
@@ -26,6 +37,23 @@ var ctrl = app.controller('ctrl', function($scope, $http) {
       $scope.currentSong = song
     }
   }
+
+  $scope.changed = function(){
+    $scope.results = null;
+  }
+
+  $scope.getTopTracks = function(artistId) {
+    $scope.url = 'https://api.spotify.com/v1/artists/' + artistId + '/top-tracks?country=US'
+    $http.get($scope.url).success(function(response){
+      toptracks = $scope.toptracks = response.tracks;
+    })
+  }
+
+
+
+
+
+
 })
 
 // Add tool tips to anything with a title property
@@ -33,12 +61,31 @@ $('body').tooltip({
     selector: '[title]'
 });
 
+//shows info on hover
+
+
 
 /*
-know the length of the sample track 
-where is currentSong initialized
-is the play() and pause() user-defined!? (not given)
-is q for query?
-how do you find the -baseURL
-search song and artist and then return the overlapped data? (matched both song and artist)
-delete duplicates
+sources---
+get an artist's top tracks
+  https://api.spotify.com/v1/artists/{id}/top-tracks?country=US
+  OR 
+  artists.items.href + '/top-tracks?country=US'
+  can get id by...
+  (using the artists search) artists.items.id
+get artist image..
+  artists.items.images[2].url
+artists.items.followers.total
+artists.items.name
+artists.items.popularity
+
+top tracks
+(search top tracks--off search bar)
+album name-- tracks.album.name
+track name-- tracks.album.external_urls.name
+track preview-- tracks.album.external_urls.preview_url
+
+
+
+
+*/
